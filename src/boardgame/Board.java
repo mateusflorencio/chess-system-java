325fd39f3f95873687821875640d28bb4a5a22ac
Mateus Florencio <mateusflorencio@outlook.com.br>
2021-11-04 16:17:08 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,11 +38,25 @@ public class Board {
     }
 
     public void placePiece(Piece piece, Position position) {
-        if (thereIsPiece(position)){
+        if (thereIsPiece(position)) {
             throw new BoardException("There is already a piece on position");
         }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+
+        return aux;
     }
 
     private boolean positionExists(int row, int column) {
@@ -50,8 +64,9 @@ public class Board {
     }
 
     public boolean positionExists(Position position) {
-        return positionExists(position.getRow(),position.getColumn());
+        return positionExists(position.getRow(), position.getColumn());
     }
+
     public boolean thereIsPiece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
--- src/chess/ChesseException.java
+++
@@ -1,8 +0,0 @@
-package chess;
-
-public class ChesseException extends RuntimeException{
-
-    public ChesseException (String msg){
-        super(msg);
-    }
-}
